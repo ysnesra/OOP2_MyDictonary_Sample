5eb90b25babade56a8a85c41e7f9a851b3d6dbc0
EsraYasin <ysnesra@hotmail.com>
2023-01-01 20:15:32 +0300

    Add project files.

---
+++ OOP2_MyDictonary_Sample/MyDictonary.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP2_MyDictonary_Sample
+{
+    class MyDictonary<T,U>
+    {
+         T[] Key;       //city dizisi key ve value olarak ayrı ayrı tanımladım
+         U[] Value;
+               
+        public MyDictonary()
+        {
+            //cities dizisini newleyerek constructor içinde oluşturdum.(başlangıçta içiboş)
+            Key = new T[0];    
+            Value = new U[0];
+        }
+
+        public void Add(T itemKey, U itemvalue)
+        {
+            //geçici dizi oluşturuyorum veriler kaybolmasın 
+            //cityKey in referans adresini tempcityKey e atıyorum.
+            T[] tempcityKey = Key;
+            U[] tempcityValue = Value;
+
+            Key = new T[Key.Length + 1];     //cities dizisinin elemen sayısını 1 artırdım
+            Value = new U[Value.Length + 1];
+
+            for (int i = 0; i < tempcityKey.Length; i++)
+            {
+                Key[i] = tempcityKey[i];    //tempcities de tuttugum elemanları cities diziisne geri alıyorum
+                Value[i] = tempcityValue[i];
+            }
+
+            //Kullanıcıdan gelen parametreyi dizinin son elemanını olarak ekliyorum
+            Key[Key.Length - 1] = itemKey;
+            Value[Value.Length - 1] = itemvalue;
+        }
+
+
+        //eleman sayısını veren metot
+        public int MyLength      
+        {
+            get { return Key.Length; }
+        }
+
+        //elemanları Listeli ekrana yazdıran metot
+        public void List()
+        {
+            foreach (var ckey in Key)
+            {
+                Console.WriteLine($"Key: {ckey}");
+            }
+            foreach (var cvalue in Value)
+            {
+                Console.WriteLine($"Value: {cvalue}");
+            }
+        }
+
+
+    }
+}
---
+++ OOP2_MyDictonary_Sample/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP2_MyDictonary_Sample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dictionary<TKey, TValue>      
+            //**key olarak tanımladığımız index numarasına göre çağırırız  
+
+            //plaka => (34: İstanbul), (35: İzmir)
+
+            MyDictonary<int, string> cities = new MyDictonary<int, string>();
+            cities.Add(34, "İstanbul");
+            cities.Add(38, "Kayseri");
+            cities.Add(35, "İzmir");
+            cities.Add(41, "Kocaeli");
+
+            Console.WriteLine(cities.MyLength);   //MyLength metotunu çağırırız
+
+            cities.List();
+       
+        }
+    }
+}
